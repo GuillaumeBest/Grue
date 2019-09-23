4b0103b9f668194612326cc935565a67eb073022
RPapin <remi.papin@viacesi.fr>
2019-09-23 15:54:18 +0200

    ws lundi

---
+++ src/controller/CLctrlCrypt.java
@@ -0,0 +1,15 @@
+package controller;
+
+public class CLctrlCrypt {
+	public void ecrireUnFichier(String path, String texte) {
+		
+	}
+	public String lireFichierSimple(String path) {
+		return path;
+		
+	}
+	public String crypter(String texte, String Key) {
+		return Key;
+		
+	}
+}
---
+++ src/model/CLfichier.java
@@ -0,0 +1,12 @@
+package model;
+
+public class CLfichier {
+	public String lireFichier(String path) {
+		return path;
+		
+	}
+	public void ecrireFichier(String path, String texte) {
+		
+	}
+
+}
