f64997e26ac7b898929e0e65bb1b2080b277610f
RPapin <remi.papin@viacesi.fr>
2019-09-24 11:07:22 +0200

    Lecture ecriture

--- src/controller/CLctrlCrypt.java
+++ src/controller/CLctrlCrypt.java
@@ -1,11 +1,42 @@
 package controller;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class CLctrlCrypt {
 	public void ecrireUnFichier(String path, String texte) {
-		
+	    try {
+			PrintWriter writer = new PrintWriter(new FileWriter(path, false));
+			writer.print(texte);
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+
 	}
 	public String lireFichierSimple(String path) {
-		return path;
+		FileReader fr;
+		String str = null;
+		try {
+			fr = new FileReader(path);
+		    str = "";
+		    int i = 0;
+		    //Lecture des données
+		    while((i = fr.read()) != -1)
+		      str += (char)i;
+
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return str;
 		
 	}
 	public String crypter(String texte, String Key) {
--- src/main/Main.java
+++ src/main/Main.java
@@ -12,34 +12,34 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		final IModel model = new Model();
-		final IView view = new View();
-		final IController controler = new Controller(model, view);
+//		final IModel model = new Model();
+//		final IView view = new View();
+//		final IController controler = new Controller(model, view);
 
 		CLctrlCrypt o1;
 		String reponse;
 		
 		o1 = new CLctrlCrypt();
-		o1.ecrireUnFichier("C:\\Users\\remip\\Desktop\\fichier\\a.txt", "Bonjour a tous. Comment Allez vous?");
+		o1.ecrireUnFichier("C:\\Users\\remip\\Desktop\\fichier\\a.txt", "Bonjour a tous. Comment ");
 		
 		reponse = o1.lireFichierSimple("C:\\Users\\remip\\Desktop\\fichier\\a.txt");
 		System.out.println(reponse);
-		
-		reponse = o1.crypter(reponse, "adhyudertjutl");
-		System.out.println(reponse);
-		o1.ecrireUnFichier("C:\\Users\\remip\\Desktop\\fichier\\b.txt", reponse);
-		
-		reponse = o1.lireFichierSimple("C:\\Users\\Emmanuel\\Desktop\\fichier\\b.txt");
-		reponse = o1.crypter(reponse, "adhyudertjutl");
-		o1.ecrireUnFichier("C:\\Users\\remip\\Desktop\\fichier\\c.txt", reponse);
-		System.out.println(reponse);
+//		
+//		reponse = o1.crypter(reponse, "adhyudertjutl");
+//		System.out.println(reponse);
+//		o1.ecrireUnFichier("C:\\Users\\remip\\Desktop\\fichier\\b.txt", reponse);
+//		
+//		reponse = o1.lireFichierSimple("C:\\Users\\Emmanuel\\Desktop\\fichier\\b.txt");
+//		reponse = o1.crypter(reponse, "adhyudertjutl");
+//		o1.ecrireUnFichier("C:\\Users\\remip\\Desktop\\fichier\\c.txt", reponse);
+//		System.out.println(reponse);
 		
 		System.out.println("Terminé");
 		
 		
 
-		controler.loadDatas();
-		controler.launchApplication();
+//		controler.loadDatas();
+//		controler.launchApplication();
 
 	}
 }
