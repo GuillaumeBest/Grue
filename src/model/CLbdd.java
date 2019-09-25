6ff6ed0103f5ee57753fe242dc494ec8f65e080e
Ssaubeck <33596354+Ssaubeck@users.noreply.github.com>
2019-09-25 15:30:40 +0200

    Bdd

--- src/model/CLbdd.java
+++ src/model/CLbdd.java
@@ -1,19 +1,45 @@
 package model;
 
+
 import java.sql.*;
+
 
 
 public class CLbdd {
 
-    public CLbdd(){
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/","root", "");
-            Statement stmt = con.createStatement();;
-            ResultSet rs= stmt.executeQuery("");
-            while(rs.next())
-                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
-            con.close();
-        } catch (Exception e) { System.out.println(e); }
+    private Connection con;
+    private Statement stmt;
+    private ResultSet rs;
+
+    public CLbdd() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/grue?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
+
+    ;
+
+    //-------------------------------------------------------
+    public String GetRows(String rq_sql, String resultSetName) {
+        try {
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(rq_sql);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return ("1");
+
+    }
+
+    ;
+
+    //-------------------------------------------------------
+    public void ActionRows(String rq_sql) {
+    }
+
+    ;
 }
